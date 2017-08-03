2f897bc04c2663b702ed683566b440bfae04402b
Taylor Price <tayworm@gmail.com>
2017-08-02 19:06:30 -0700

    set up symlinks

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
+ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin
+ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout
+ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
+ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
+ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
+ln -s ~/.zprezto/runcoms/.gitconfig ~/.gitconfig
