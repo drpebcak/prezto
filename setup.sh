6d2769a57981027852416a046c044e8a99a8e0a1
Taylor Price <tayworm@gmail.com>
2020-10-13 11:12:13 -0700

    sync changes

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
 ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
 ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
 ln -s ~/.zprezto/runcoms/.gitconfig ~/.gitconfig
+ln -s ~/.zprezto/runcoms/p10k.zsh ~/.p10k.zsh
 
 # Sync helper scripts
 ln -s ~/.zprezto/runcoms/scripts/gitlab-repo-sync /usr/local/bin/gitlab-repo-sync
